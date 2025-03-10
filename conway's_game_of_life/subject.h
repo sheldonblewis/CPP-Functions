305f38e6949b09e55565323f6f79103d6bb51681
Sheldon Lewis <sheldonblewis@gmail.com>
2025-03-10 17:13:41 -0400

    Create subject.h

---
+++ conway's_game_of_life/subject.h
@@ -0,0 +1,22 @@
+#ifndef SUBJECT_H
+#define SUBJECT_H
+#include <vector>
+#include <string>
+#include "info.h"
+
+class Observer;
+
+class Subject {
+    std::vector<Observer*> observers;
+
+  public:
+    void attach( Observer *o );  
+    void notifyObservers();
+    virtual Info getInfo() const = 0;
+
+    // Returns a string consisting of the name of each observer, followed by a single space.
+    // If there are no observers, an empty string is returned.
+    std::string getObserverNames();
+};
+
+#endif
